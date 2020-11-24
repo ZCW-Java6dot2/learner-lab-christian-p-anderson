32510cdfdbc87ca9537c8059fba72adc6c7db5a4
christian <christian@zipcodes-MBP-51.lan>
2020-11-24 08:01:07 -0500

    configed pom file and completed Part 1

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -2,4 +2,18 @@ package io.zipcoder.interfaces;
 
 public class Person {
 
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,31 @@
 package io.zipcoder.interfaces;
 
+import org.junit.jupiter.api.Test;
+
+import static org.testng.Assert.*;
+
 public class TestPerson {
+
+    private Person testPerson;
+
+    @Test
+    public void testConstructor() {
+        testPerson = new Person("John");
+        assertEquals("John", testPerson.getName());
+    }
+
+    @Test
+    public void testSetName() {
+        // When
+        String expected = "Charlie";
+        Person testPerson = new Person(expected);
+
+        // Given
+        String actual = testPerson.getName();
+
+        // Then
+        assertEquals(expected, actual);
+    }
+
 
 }
