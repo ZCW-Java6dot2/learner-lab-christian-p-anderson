c6bba3bfc3e787ef82d2a301dd4ca1394e7c3aa4
christian <christian@zipcodes-MBP-51.lan>
2020-11-25 20:52:16 -0500

    completed Part 8.1

--- src/main/java/io/zipcoder/interfaces/ClassRoom.java
+++ src/main/java/io/zipcoder/interfaces/ClassRoom.java
@@ -1,5 +1,20 @@
 package io.zipcoder.interfaces;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ClassRoom {
 
+    private ArrayList<Student> students = TechConnect.getStudents();
+    private ArrayList<Instructor> instructors = ZipCodeWilmington.getInstructors();
+
+    public HashMap<Student, Instructor> getRoster(ArrayList<Student> students, ArrayList<Instructor> instructors) {
+
+        HashMap<Student, Instructor> a = new HashMap<Student, Instructor>();
+        for (int i = 0; i < students.size(); i++) {
+            a.put(students.get(i), instructors.get(i));
+        }
+
+        return a;
+    }
 }
